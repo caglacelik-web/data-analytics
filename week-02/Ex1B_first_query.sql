5818acfb410f980839d9ddd92b93cf48df15f332
Çağla Çelik <caglacelik@Cagla-MacBook-Air.local>
2026-04-13 15:59:37 -0500

    Added queries of actor and film tables

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,15 @@
+/*
+Sakila is a database used to collect information about a movie rental business and its customers for business operations and reporting.
+*/
+
+USE sakila;
+SELECT * FROM actor; 
+/* 
+Retrieved 200 records
+*/
+
+SELECT * FROM film;
+/* 
+Retrieved 1000 rows
+*/
+
