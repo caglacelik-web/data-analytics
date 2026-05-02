cd6f06fc0dc005ca8bdd980529747f783d11cdd8
caglacelik-web <cagla.celik23@gmail.com>
2026-05-02 13:26:51 -0500

    Ex1A Complete

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,19 @@
 print("Hello, World!")
+
+message = "Hello, World!"
+print(message)
+# "Hello world!" prints twice because there are two print statements:
+# one on line 1 and one using the message variable
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+# The result is 3.5 
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
